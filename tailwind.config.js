92350ed68012da3b34eda54c4ae97450cf7e25c7
David Dossett <ddossett@microsoft.com>
2022-02-05 22:19:23 -0800

    Set up tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  purge: [
-    // Use *.tsx if using TypeScript
-    './pages/**/*.tsx',
-    './components/**/*.tsx',
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
