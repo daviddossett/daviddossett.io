23a72a0d039bc52fa44757a4fc8302a50624468f
David Dossett <ddossett@microsoft.com>
2024-03-25 08:40:56 -0700

    Fix mobile padding

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 
 function Layout({ children }: any): JSX.Element {
   return (
-    <div className='max-w-[720px] mx-auto w-fullpx-4 py-28 pb-10 md:px-8'>
+    <div className='max-w-[720px] mx-auto w-full px-4 py-5 md:py-28 pb-10 md:px-8 pl-4 pr-4'>
       <Head>
         <title>David Dossett</title>
         <meta name='description' content='My website' />
