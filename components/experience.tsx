d2f44ee82e03158ece5c008e88c867f257c6ee43
David Dossett <ddossett@microsoft.com>
2024-06-08 21:43:14 -0700

    Refactor experiences to use an array mapped to components

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { DescriptionRow } from "./description-row";
 
 interface ExperienceProps {
@@ -20,6 +21,39 @@ const Experience: React.FC<ExperienceProps> = ({
   );
 };
 
+const experiences = [
+  {
+    title: "Principal Designer, VS Code",
+    company: "Microsoft",
+    description: "2024 — Present",
+  },
+  {
+    title: "Senior Designer, VS Code",
+    company: "Microsoft",
+    description: "2021 — 2024",
+  },
+  {
+    title: "Senior Designer, Product Insights",
+    company: "Microsoft",
+    description: "2019 — 2021",
+  },
+  {
+    title: "Designer 2, Aria",
+    company: "Microsoft",
+    description: "2018 — 2019",
+  },
+  {
+    title: "Designer, Aria",
+    company: "Microsoft",
+    description: "2016 — 2018",
+  },
+  {
+    title: "Intern, HoloLens",
+    company: "8ninths",
+    description: "2015",
+  },
+];
+
 export const ExperienceList: React.FC = () => {
   return (
     <div className="flex-col">
@@ -27,36 +61,14 @@ export const ExperienceList: React.FC = () => {
         Experience
       </h2>
       <div className="flex flex-col gap-6">
-        <Experience
-          title="Principal Designer, VS Code"
-          company="Microsoft"
-          description="2024 — Present"
-        />
-        <Experience
-          title="Senior Designer, VS Code"
-          company="Microsoft"
-          description="2021 — 2024"
-        />
-        <Experience
-          title="Senior Designer, Product Insights"
-          company="Microsoft"
-          description="2019 — 2021"
-        />
-        <Experience
-          title="Designer 2, Aria"
-          company="Microsoft"
-          description="2018 — 2019"
-        />
-        <Experience
-          title="Designer, Aria"
-          company="Microsoft"
-          description="2016 — 2018"
-        />
-        <Experience
-          title="Intern, HoloLens"
-          company="8ninths"
-          description="2015"
-        />
+        {experiences.map((experience, index) => (
+          <Experience
+            key={index}
+            title={experience.title}
+            company={experience.company}
+            description={experience.description}
+          />
+        ))}
       </div>
     </div>
   );
