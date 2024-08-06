2d83b774fa3fb91da3af10603bd421157d18c4fd
David Dossett <ddossett@microsoft.com>
2024-08-06 10:31:45 -0700

    Remove padding override on larger screens

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,7 +3,7 @@ import { Header } from "./header";
 
 function Layout({ children }: any): JSX.Element {
   return (
-    <div className="max-w-[960px] mx-auto w-full px-4 md:px-8">
+    <div className="max-w-[960px] mx-auto w-full px-4">
       <Head>
         <title>David Dossett</title>
         <meta name="description" content="Product designer at Microsoft"/>
